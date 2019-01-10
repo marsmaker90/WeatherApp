150b3c0f5101e2dbb51f91c9bffd9b6c1a3cfd57
maniarasan <marsmaker90@gmail.com>
2019-01-10 19:54:57 +0530

    Created - Test case to validate the Weather report

---
+++ app/src/test/java/com/poc/weatherapp/WeatherReportUnitTest.kt
@@ -0,0 +1,15 @@
+package com.poc.weatherapp
+
+import org.junit.Test
+
+
+class WeatherReportUnitTest {
+
+    /**
+     * To test the current weather
+     */
+    @Test
+    fun testWeatherReport() {
+        assert(false)
+    }
+}
