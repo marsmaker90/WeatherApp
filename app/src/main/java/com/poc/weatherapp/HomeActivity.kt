6a3b4268a2cb3b6841a97bde70914d56e4c777fa
maniarasan <marsmaker90@gmail.com>
2019-01-10 19:39:57 +0530

    Migrating the base project to AndroidX - To support material components

--- app/src/main/java/com/poc/weatherapp/HomeActivity.kt
+++ app/src/main/java/com/poc/weatherapp/HomeActivity.kt
@@ -1,7 +1,7 @@
 package com.poc.weatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
 
